48e3fbd84efc589251800fa822537b97726bd065
Manuel <manuel.asto.lazaro@gmail.com>
2024-10-24 20:15:31 -0500

    Update - Se agrega imagenes

--- resources/views/partials/website/navbar.blade.php
+++ resources/views/partials/website/navbar.blade.php
@@ -9,31 +9,10 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <div class="navbar-nav ms-auto py-0">
                 <a href="{{ route('website.index') }}" class="nav-item nav-link {{ isActive('website.index') }}">Home</a>
-                <a href="{{ route('catalogo.index') }}" class="nav-item nav-link {{ isActive('catalogo.index') }}">Catálogo</a>
+                <a href="https://tienda.devesoft.tech" target="_blank" class="nav-item nav-link">Tienda</a>
                 <a href="{{ route('website.software') }}" class="nav-item nav-link {{ isActive('website.software') }}">Software</a>
-                <!-- <a href="#" class="nav-item nav-link">Nosotros</a>
-                <a href="#" class="nav-item nav-link">Servicios</a> -->
-                <!-- <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Blog</a>
-                        <div class="dropdown-menu m-0">
-                            <a href="blog.html" class="dropdown-item">Blog Grid</a>
-                            <a href="detail.html" class="dropdown-item">Blog Detail</a>
-                        </div>
-                    </div> -->
-                <!-- <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                        <div class="dropdown-menu m-0">
-                            <a href="price.html" class="dropdown-item">Pricing Plan</a>
-                            <a href="feature.html" class="dropdown-item">Our features</a>
-                            <a href="team.html" class="dropdown-item">Team Members</a>
-                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                            <a href="quote.html" class="dropdown-item">Cotización Gratis</a>
-                        </div>
-                    </div> -->
-                <!-- <a href="#" class="nav-item nav-link">Contáctanos</a> -->
             </div>
-            <butaton type="button" class="btn text-primary ms-3" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa fa-search"></i></butaton>
-            <a href="{{ route('login') }}" class="btn btn-primary py-2 px-4 ms-3">Log In</a>
+            <a target="_blank" href="{{ asset('BROCHURE_DEVESOFT.pdf') }}" class="btn btn-primary py-2 px-4 ms-3">BROCHURE</a>
         </div>
     </nav>
     <!-- Navbar & Carousel End -->
